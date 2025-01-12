bbf088f8ca45f1b7b80d2c15ca579bfae72a5726
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2025-01-13 02:21:18 +0900

    :truck: 초기 페이지 라우팅 설정

---
+++ src/app/mentorings/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Params } from 'next/dist/server/request/params';
+
+export default async function Page(props: { params: Params }) {
+  const { id } = await props.params;
+
+  return(
+    <div>
+      {id as string}번 멘토링 상세 페이지
+    </div>
+  )
+}
---
+++ src/app/mypage/card-settings/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return(
+    <div>
+      마이 페이지 명함 설정
+    </div>
+  )
+}
