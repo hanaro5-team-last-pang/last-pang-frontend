c04084de259f8bd1d1b4b83e1f77aad42498d9d8
kkx7787 <kkx7787@naver.com>
2025-01-15 14:17:25 +0900

    :fire: 메인 페이지 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import MyPageNavbar from '@/components/organisms/MyPageNavbar';
-
 export default function Home() {
   return (
-    <div>
-      <MyPageNavbar />
+    <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      하나은행
     </div>
   );
 }
