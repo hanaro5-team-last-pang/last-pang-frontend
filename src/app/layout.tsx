44565a4f715a2911b4f3654dfd68bb59cc96fd5f
kkx7787 <kkx7787@naver.com>
2025-01-16 15:28:09 +0900

    :lipstick: 프로젝트 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,5 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { ReactNode } from 'react';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
-export const metadata: Metadata = {
-  title: '하나학당',
-  description: '금융 멘토링 서비스 하나학당입니다.',
-};
 
 export default function RootLayout({
   children,
@@ -25,11 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="font-fontMedium">{children}</body>
     </html>
   );
 }
