afe47407bac54f64e26defbe25a2bc02223262ad
6jieun <mjkim12160@gmail.com>
2025-01-16 10:33:10 +0900

    :sparkles: 카테고리 버튼 컴포넌트 추가, 유기체 컴포넌트에 아이콘 뱃지 원자 컴포넌트 적용

--- src/components/organisms/CardView.tsx
+++ src/components/organisms/CardView.tsx
@@ -1,9 +1,13 @@
 import { Badge } from '@/components/atoms/Badge';
+import IconBadge from '@/components/atoms/IconBadge';
 import { FaRegCalendar, FaRegClock } from 'react-icons/fa6';
 import { IoPerson } from 'react-icons/io5';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface CardViewProps {
   title: string;
+  imageSrc: string;
   //멘토링 목록의 prop
   mentor_name?: string;
   start_time?: string;
@@ -14,10 +18,12 @@ interface CardViewProps {
   //금융뉴스카드의 prop
   date?: string;
   description?: string;
+  route: string;
 }
 
 export default function CardView({
   title,
+  imageSrc,
   mentor_name,
   start_time,
   duration,
@@ -26,58 +32,77 @@ export default function CardView({
   category,
   date,
   description,
+  route,
 }: CardViewProps) {
   return (
-    <div className="flex flex-col h-full border rounded-2xl shadow-md overflow-hidden">
-      <div className="relative w-full h-1/2 bg-ourGreen text-white flex items-center justify-center">
-        <div>
-          {category && (
-            <Badge
-              text={category}
-              className="rounded-lg absolute top-3 left-3 z-10 bg-red-500"
-            />
+    <Link href={route} className={'cursor-pointer'}>
+      <div className="flex flex-col w-1/2 border rounded-2xl shadow-md overflow-hidden aspect-[3/4]">
+        <div className="relative h-3/5 text-white flex items-center justify-center">
+          <div>
+            {category && (
+              <Badge
+                text={category}
+                className="rounded-lg absolute top-3 left-3 z-10 bg-red-500"
+              />
+            )}
+          </div>
+          <Image src={imageSrc} alt={''} fill />
+        </div>
+        <div className="flex-1 p-4">
+          <div className="text-xl font-bold mb-1">{title}</div>
+
+          {mentor_name && (
+            <div className="text-gray-700 mb-2">{mentor_name} 멘토</div>
+          )}
+          {date && (
+            <div className="flex items-center mb-1">
+              <IconBadge
+                icon={
+                  <FaRegCalendar className="text-ourGreen text-lg font-bold" />
+                }
+                text={date}
+                gapLength="2"
+                textClassName="text-gray-700"
+              />
+            </div>
+          )}
+
+          {description && <div className="text-gray-700">{description}</div>}
+
+          {start_time && (
+            <div className="flex items-center mb-1">
+              <IconBadge
+                icon={
+                  <FaRegCalendar className="text-ourGreen text-lg font-bold" />
+                }
+                text={start_time}
+                gapLength="2"
+                textClassName="text-gray-700 mr-3"
+              />
+              {duration !== undefined && (
+                <IconBadge
+                  icon={
+                    <FaRegClock className="text-ourGreen text-lg font-bold" />
+                  }
+                  text={`${duration}시간`}
+                  gapLength="2"
+                  textClassName="text-gray-700 mr-3"
+                />
+              )}
+              {participants !== undefined && max_participants !== undefined && (
+                <IconBadge
+                  icon={
+                    <IoPerson className="text-ourGreen text-lg font-bold" />
+                  }
+                  text={`${participants}/${max_participants}`}
+                  gapLength="2"
+                  textClassName="text-gray-700 mr-3"
+                />
+              )}
+            </div>
           )}
         </div>
-        <img
-          src="/" // 이미지 경로
-          className="w-full h-full object-cover"
-          alt="이벤트 이미지"
-        />
       </div>
-      <div className="flex-1 p-4">
-        <div className="text-xl font-bold mb-1">{title}</div>
-
-        {mentor_name && (
-          <div className="text-gray-700 mb-2">{mentor_name} 멘토</div>
-        )}
-        {date && (
-          <div className="flex items-center text-gray-700 mb-2">
-            <FaRegCalendar className="mr-1 text-ourGreen text-lg font-bold" />
-            {date}{' '}
-          </div>
-        )}
-        {description && <div className="text-gray-700 mb-2">{description}</div>}
-
-        {start_time && (
-          <div className="flex items-center text-gray-700 mb-1">
-            <FaRegCalendar className="mr-1 text-ourGreen text-lg font-bold" />
-            {start_time}{' '}
-            {duration !== undefined && (
-              <>
-                <FaRegClock className="ml-3 mr-1 text-ourGreen text-lg font-bold" />
-                {duration}시간{' '}
-              </>
-            )}
-            {participants !== undefined && max_participants !== undefined && (
-              <>
-                {' '}
-                <IoPerson className="ml-3 mr-1 text-ourGreen text-lg font-bold" />
-                {participants}/{max_participants}
-              </>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
+    </Link>
   );
 }
