75171fb33f52fbff18391c68493d53201813dce3
6jieun <mjkim12160@gmail.com>
2025-01-16 11:04:53 +0900

    🐛 버그 수정

--- src/stories/CardView.stories.tsx
+++ src/stories/CardView.stories.tsx
@@ -13,7 +13,7 @@ export const Default: StoryObj = {
       title="금융뉴스 제목"
       date="2025-01-20"
       description="금융 뉴스 요약 설명 들어가는 곳"
-      route="/mypage"
+      route="/login"
     />
   ),
 };
