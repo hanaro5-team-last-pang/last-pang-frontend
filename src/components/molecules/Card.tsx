b7728310f8b909c752ce8f33306cd458facaaa6c
kkx7787 <kkx7787@naver.com>
2025-01-21 17:49:22 +0900

    :triangular_flag_on_post: 금융 상품 카드 onClick 이벤트 추가

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -6,27 +6,49 @@ interface CardProps {
   cardTitle: string;
   cardDescription: string;
   cardImage: string;
+  cardLinkUrl?: string;
 }
 export default function Card({
   cardColor,
   cardTitle,
   cardDescription,
   cardImage,
+  cardLinkUrl,
 }: CardProps) {
+  const navigate = () => {
+    window.open(cardLinkUrl, '_blank');
+  };
+
   return (
     <div className="w-80 h-36 p-2">
-      <div
-        className={clsx(
-          'rounded-lg drop-shadow-md p-3 flex flex-col h-full',
-          cardColor
-        )}
-      >
-        <div className="text-xl font-bold">{cardTitle}</div>
-        <div className="ml-1 my-1 font-fontLight">{cardDescription}</div>
-        <div className="absolute h-20 w-20 right-0 bottom-0">
-          <Image src={cardImage} alt="카드 이미지" fill />
+      {!cardLinkUrl ? (
+        <div
+          className={clsx(
+            'rounded-lg drop-shadow-md p-3 flex flex-col h-full',
+            cardColor
+          )}
+        >
+          <div className="text-xl font-bold">{cardTitle}</div>
+          <div className="ml-1 my-1 font-fontLight">{cardDescription}</div>
+          <div className="absolute h-20 w-20 right-0 bottom-0">
+            <Image src={cardImage} alt="카드 이미지" fill />
+          </div>
         </div>
-      </div>
+      ) : (
+        <button
+          className={clsx(
+            'rounded-lg drop-shadow-md p-3 flex flex-col h-full',
+            cardColor
+          )}
+          onClick={() => navigate}
+        >
+          <div className="text-xl font-bold">{cardTitle}</div>
+          <div className="ml-1 my-1 font-fontLight">{cardDescription}</div>
+          <div className="absolute h-20 w-20 right-0 bottom-0">
+            <Image src={cardImage} alt="카드 이미지" fill />
+          </div>
+        </button>
+      )}
     </div>
   );
 }
